f5daa965c88a3254ce4c30b4baf1e50c8133ca2e
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2025-01-04 10:50:22 +0300

    feat: Containerize and local orchestration

--- services/notification-service/server.js
+++ services/notification-service/server.js
@@ -11,7 +11,7 @@ const app = require('./app')
 // ...Other middleware and route configurations...
 
 // Start the server
-const PORT = process.env.PORT || 4005;
+const PORT = process.env.PORT || 5006;
 
 app.listen(PORT, async () => {
   try {
--- services/order-service/server.js
+++ services/order-service/server.js
@@ -11,7 +11,7 @@ const app = require('./app')
 // ...Other middleware and route configurations...
 
 // Start the server
-const PORT = process.env.PORT || 4003;
+const PORT = process.env.PORT || 5005;
 
 app.listen(PORT, async () => {
   try {
